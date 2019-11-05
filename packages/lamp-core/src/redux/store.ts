d4c51299e4cba461250322f0dd6f2fcd99687d1d
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-11-05 11:01:56 +0800

    use redux devtool

--- packages/lamp-core/src/redux/store.ts
+++ packages/lamp-core/src/redux/store.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
 import reducer from './reducers'
@@ -6,9 +6,14 @@ import mySaga from './sagas'
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 // mount it on the Store
-export default createStore(reducer, applyMiddleware(sagaMiddleware))
+export default createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+)
 
 // then run the saga
 sagaMiddleware.run(mySaga)
